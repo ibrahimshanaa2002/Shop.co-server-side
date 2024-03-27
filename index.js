8ba155ac333119395c75b77dad8dcb393ba1eb91
ibrahimshanaa2002 <132352098+ibrahimshanaa2002@users.noreply.github.com>
2024-03-28 00:58:43 +0200

    host

--- index.js
+++ index.js
@@ -15,6 +15,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/test", (req, res) => {
+  res.json({ msg: "You app is running" });
+});
+
 // Mount userRoute
 app.use("/api/user", userRoute);
 app.use("/api/product", productRoute);
